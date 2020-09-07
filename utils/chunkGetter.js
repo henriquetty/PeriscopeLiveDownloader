1f738e7b67c668248b770571bb7a4123fa89eec8
henriquetty <gamercaipira@gmail.com>
2020-09-07 16:08:51 -0300

    updating request library

--- utils/chunkGetter.js
+++ utils/chunkGetter.js
@@ -2,10 +2,12 @@ const request = require("request");
 const downloadChunk = require("./liveDownloader");
 
 //dynamic url
-const url = "";
+const url =
+  "https://prod-fastly-eu-central-1.video.pscp.tv/Transcoding/v1/hls/w_ClYb0w9m4KYpvY33XcgkApwltGsYfC0IY2T5wFJkCP3Ds7Bc3nKITj46O8R8OBAo739ycd0vLkr9tnJm2nMQ/non_transcode/eu-central-1/periscope-replay-direct-prod-eu-central-1-public/dynamic_playlist.m3u8?type=live";
 
 //chunk url
-const chunkUrl = "";
+const chunkUrl =
+  "https://prod-fastly-eu-central-1.video.pscp.tv/Transcoding/v1/hls/w_ClYb0w9m4KYpvY33XcgkApwltGsYfC0IY2T5wFJkCP3Ds7Bc3nKITj46O8R8OBAo739ycd0vLkr9tnJm2nMQ/non_transcode/eu-central-1/periscope-replay-direct-prod-eu-central-1-public/";
 
 let arrChunk = [];
 
