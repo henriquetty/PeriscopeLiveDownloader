7a494dee06afda7b6047fd02cc1fca7b73fb8c03
henriquetty <gamercaipira@gmail.com>
2020-09-07 16:04:12 -0300

    updating yargs

--- utils/chunkGetter.js
+++ utils/chunkGetter.js
@@ -1,30 +1,32 @@
-const request = require('request');
-const downloadChunk = require('./liveDownloader');
+const request = require("request");
+const downloadChunk = require("./liveDownloader");
 
 //dynamic url
-const url = ''
+const url = "";
 
 //chunk url
-const chunkUrl = ''
+const chunkUrl = "";
 
 let arrChunk = [];
 
 setInterval(() => {
-    request.get(url, (error, response) => {
+  request.get(url, (error, response) => {
+    let chunk = response.body.split(",");
+    let lastIndexChunk = chunk.length - 1;
 
-        let chunk = response.body.split(',');
-        let lastIndexChunk = chunk.length - 1
+    if (
+      arrChunk.includes(chunk[lastIndexChunk]) ||
+      chunk[lastIndexChunk] == "Not Found\n"
+    ) {
+      console.log("Not pushing due because item is already in.");
+    } else {
+      if (arrChunk.length >= 5) {
+        downloadChunk(chunkUrl, arrChunk);
+        arrChunk = [];
+      }
+      arrChunk.push(chunk[lastIndexChunk]);
+    }
 
-        if (arrChunk.includes(chunk[lastIndexChunk]) || chunk[lastIndexChunk] == 'Not Found\n') {
-            console.log('Not pushing due because item is already in.');
-        } else {
-            if (arrChunk.length >= 5) {
-                downloadChunk(chunkUrl, arrChunk);
-                arrChunk = [];
-            }
-            arrChunk.push(chunk[lastIndexChunk]);
-        }
-
-        console.log(arrChunk);
-    });
+    console.log(arrChunk);
+  });
 }, 1500);
